9fd53f7ef780833745899295cce44ba98841cca4
Vishwa Vijaysheel <vishwa7509@outlook.com>
2026-05-01 16:58:53 +1000

    Iphone night mode bg fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -185,3 +185,14 @@ setInterval(() => {
     }
   });
 })();
+
+
+const themeToggle = document.getElementById('themeToggle');
+
+themeToggle.addEventListener('change', function() {
+  if (this.checked) {
+    document.body.classList.add('dark');
+  } else {
+    document.body.classList.remove('dark');
+  }
+});
