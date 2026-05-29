4b40ab78c7a80e18438cf19193eca92b55ad11c0
aarav7509 <aarav7509@gmail.com>
2026-05-29 22:29:34 +1000

    Initial Exploration

--- assets/FirstLLM.py
+++ assets/FirstLLM.py
@@ -1,8 +1,6 @@
 # Reading the dataset
 import pandas as pd
-df = pd.read_csv("hf://datasets/dazzle-nu/CIS435-CreditCardFraudDetection/fraudTrain.csv")
-print(df.info())
-print(df.head(10))
+
 
 # Data Exploration
 
